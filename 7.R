6951887019cc006b57be490f89378e601c3507cd
deadsmond <01.lewicki@gmail.com>
2020-04-22 12:47:05 +0200

    #7 update

--- 7.R
+++ 7.R
@@ -1,13 +1,17 @@
 # Zajêcia 7 ####
 
+# http://enroute.pl/testy-statystyczne/
+# https://www.statystyczny.pl/hipotezy-statystyczne/
+
+
 # Zadanie 1 ---------------------------------------------------------------
 x <- c(862, 870, 876, 866, 871, 865, 861, 873, 871, 872)
-shapiro <- shapiro.test(x)
-p_val <- shapiro$p.value
 
-p_val
+shapiro.test(x)$p.value
 
 mean(x)
+
+## [1] 0.2136555
 
 # Zadanie 2 ---------------------------------------------------------------
 data_A = c(78.2, 78.5, 75.6, 78.5, 78.5, 77.4, 76.6)
@@ -25,12 +29,11 @@ shapiro.test(data_B)$p.value
 qqnorm(data_B)
 qqline(data_B)
 
-
 var(data_A)
 
 var(data_B)
 
-# 0.3683809
+var.test(data_A, data_B, alternative = 'less')$p.value
 
 mean(data_A)
 
@@ -40,52 +43,54 @@ t.test(data_A, data_B, var.equal = TRUE, alternative = 'greater')$p.value
 
 # Zadanie 3 ---------------------------------------------------------------
 
-a <- c(84, 87, 87, 90, 90, 90, 90, 93, 93, 96)
-b <- c(89, 92, 98, 95, 95, 92, 95, 92, 98, 101)
+data_przed <- c(84, 87, 87, 90, 90, 90, 90, 93, 93, 96)
+data_po <- c(89, 92, 98, 95, 95, 92, 95, 92, 98, 101)
 
-boxplot(a, b)
+boxplot(data_przed, data_po)
 
-## [1] 0.7025892
+shapiro.test(data_przed)$p.value
 
-qqnorm(a)
-qqline(a)
+qqnorm(data_przed)
+qqline(data_przed)
 
-## [1] 0.691489
+shapiro.test(data_po)$p.value
 
-qqnorm(b)
-qqline(b)
+qqnorm(data_po)
+qqline(data_po)
 
-mean(a)
+mean(data_przed)
 
-mean(b)
+mean(data_po)
 
 ## [1] 0.0003786878
 
 
 # Zadanie 4 ---------------------------------------------------------------
 
-a <- c(171, 176, 179, 189, 176, 182, 173, 179, 184, 186, 189, 167, 177)
-b <- c(161, 162, 163, 162, 166, 164, 168, 165, 168, 157, 161, 172)
+wzrost_m <- c(171, 176, 179, 189, 176, 182, 173, 179, 184, 186, 189, 167, 177)
+wzrost_k <- c(161, 162, 163, 162, 166, 164, 168, 165, 168, 157, 161, 172)
 
-boxplot(a, b)
+boxplot(wzrost_m, wzrost_k)
 
-## [1] 0.8595396
+shapiro.test(wzrost_m)$p.value
 
-qqnorm(a)
-qqline(a)
+qqnorm(wzrost_m)
+qqline(wzrost_m)
 
-## [1] 0.9447828
+shapiro.test(wzrost_k)$p.value
 
-qqnorm(b)
-qqline(b)
+qqnorm(wzrost_k)
+qqline(wzrost_k)
 
-## [1] 45.74359
-## [1] 16.08333
-## [1] 0.04689163
+var(wzrost_m)
 
-mean(a)
+var(wzrost_k)
 
-mean(b)
+var.test(wzrost_m, wzrost_k, var.equal = TRUE, alternative = 'greater')$p.value
+
+mean(wzrost_m)
+
+mean(wzrost_k)
 
 ## [1] 6.928802e-07
 
